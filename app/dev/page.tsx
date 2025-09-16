8611de6afdacc434b08f216472d26520cd7205ce
dem4ron <demaaron88@gmail.com>
2025-09-16 15:12:17 +0200

    Add more eslint rules

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -22,8 +22,8 @@ export default function DevPage() {
 
         <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-4">
           <p className="text-sm text-yellow-800">
-            <strong>Note:</strong> This page is only accessible in development
-            mode. It will return a 404 in production.
+            <strong>Note:</strong> This page is only accessible in development mode. It will return
+            a 404 in production.
           </p>
         </div>
       </div>
